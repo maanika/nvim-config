0275ba19e82feb030ff9f332dff7b0b4a71df9a9
maanika <kennethmaanika@gmail.com>
2026-01-25 08:49:56 +1030

    add file structure
    - neotree

--- init.lua
+++ init.lua
@@ -4,6 +4,16 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
 
+-- Disable arrow keys in all relevant modes
+local modes = { "n", "i", "v", "s" }
+local keys = { "<Up>", "<Down>", "<Left>", "<Right>" }
+
+for _, mode in pairs(modes) do
+  for _, key in pairs(keys) do
+    vim.keymap.set(mode, key, "<nop>", { noremap = true, silent = true })
+  end
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -31,6 +41,16 @@ local plugins = {
     lazy = false,
     build = ':TSUpdate'
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+  },
 }
 
 require("lazy").setup(plugins, opts)
@@ -39,11 +59,15 @@ require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 
 -- npm install -g tree-sitter-cli@0.25.0
 require'nvim-treesitter'.setup {
   install_dir = vim.fn.stdpath('data') .. '/site'
 }
 require'nvim-treesitter'.install { 'lua', 'vim', 'vimdoc','c', 'cpp' }
+
+--keymap for Neotree
+vim.keymap.set('n', '<C-n>', ':Neotree toggle right<CR>')
+
