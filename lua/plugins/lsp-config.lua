809127a4448bc6e0f72a867c6d9fcedb15542e60
maanika <kennethmaanika@gmail.com>
2026-01-25 18:23:22 +1030

    add c/c++ language server and config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls" },
+        ensure_installed = { "lua_ls", "clangd" },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
@@ -14,15 +14,28 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      vim.lsp.config('lua_ls', { settings = {} })
-      vim.lsp.enable('lua_ls')
-
+      -- keymaps
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
       vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
+
+      -- Lua
+      vim.lsp.config('lua_ls', { settings = {} })
+      vim.lsp.enable('lua_ls')
+
+      -- C / C++
+      vim.lsp.config("clangd", {
+        cmd = {
+          "clangd",
+          "--background-index",
+          --"--clang-tidy",
+          "--completion-style=detailed",
+        },
+      })
+      vim.lsp.enable('clangd')
+
     end
   },
 }
